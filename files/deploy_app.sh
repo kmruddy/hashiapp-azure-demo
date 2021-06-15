6141dab06f260ae91a45f0cba694f1c3351fc8f2
kmruddy <kmruddy@gmail.com>
2021-06-15 14:10:46 -0400

    Initial Commit

---
+++ files/deploy_app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to deploy a very simple web application.
+# The web app has a customizable image and some text.
+
+cat << EOM > /var/www/html/index.html
+<html>
+  <head><title>Hello Everyone!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+  <!-- BEGIN -->
+  <center><img src="${URL}"></img></center>
+  <center><h1>${} says "Hello Everyone!" from Terraform ${WATFVER}!</h1></center>
+  <center><h3>Sourcing Terraform ${RGTFVER} state, Resource Group Name: ${PREFIX}</h3></center>
+  <center><h3>Sourcing Terraform ${NTFVER} state, FQDN: ${FQDN}</h3></center>
+  <center><h3>Sourcing Terraform ${VMTFVER} state, the VM's name: ${VMNAME}</h3></center>
+  <!-- END -->
+  
+  </div>
+  </body>
+</html>
+EOM
+
+echo "Script complete."
---
+++ main.tf
@@ -0,0 +1,77 @@
+provider "random" {}
+
+resource "random_shuffle" "happ_name" {
+  input        = ["Vagrant", "Packer", "Terraform", "Vault", "Nomad", "Consul", "Waypoint", "Boundary"]
+  result_count = 1
+}
+
+data "terraform_remote_state" "haarg" {
+  backend = "remote"
+
+  config = {
+    organization = "TPMM-Org"
+    workspaces = {
+      name = "hashiapp-azure-resourcegroup"
+    }
+  }
+}
+
+data "terraform_remote_state" "haan" {
+  backend = "remote"
+
+  config = {
+    organization = "TPMM-Org"
+    workspaces = {
+      name = "hashiapp-azure-network"
+    }
+  }
+}
+
+data "terraform_remote_state" "haavm" {
+  backend = "remote"
+
+  config = {
+    organization = "TPMM-Org"
+    workspaces = {
+      name = "hashiapp-azure-vm"
+    }
+  }
+}
+
+resource "null_resource" "configure-happ" {
+  triggers = {
+    build_number = timestamp()
+  }
+
+  provisioner "file" {
+    source      = "files/"
+    destination = "/home/${data.terraform_remote_state.haavm.outputs.credentials.username}/"
+
+    connection {
+      type     = "ssh"
+      user     = data.terraform_remote_state.haavm.outputs.credentials.username
+      password = data.terraform_remote_state.haavm.outputs.credentials.password
+      host     = data.terraform_remote_state.haan.outputs.happ_fqdn
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl start apache2",
+      "sudo chown -R ${data.terraform_remote_state.haavm.outputs.credentials.username}:${data.terraform_remote_state.haavm.outputs.credentials.username} /var/www/html",
+      "chmod +x *.sh",
+      "URL=${var.hashi_products[random_shuffle.happ_name.result]} HAPP=${random_shuffle.happ_name.result} PREFIX=${data.terraform_remote_state.haarg.outputs.rg_name} FQDN=${data.terraform_remote_state.haan.outputs.happ_fqdn} VMNAME=${data.terraform_remote_state.haavm.outputs.vmname} ./deploy_app.sh",
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = data.terraform_remote_state.haavm.outputs.credentials.username
+      password = data.terraform_remote_state.haavm.outputs.credentials.password
+      host     = data.terraform_remote_state.haan.outputs.happ_fqdn
+    }
+  }
+}
+
+output "happ_fqdn" {
+  value = data.terraform_remote_state.hcan.outputs.happ_fqdn
+}
